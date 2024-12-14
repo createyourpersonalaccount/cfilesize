d3e11b89ae3b60f8f6a7452c6c8caab28770e6f0
Nikolaos Chatzikonstantinou <nchatz314@gmail.com>
2024-12-14 00:17:25 -0500

    resolve symbolic links

--- src/cfilesize_unix.c
+++ src/cfilesize_unix.c
@@ -1,11 +1,20 @@
 #include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 
 #include <cfilesize/private/unix.h>
 
 off_t cfilesize_unix(const char *filename, int *error) {
   struct stat statbuf;
-  if (stat(filename, &statbuf) == -1) {
+  char path[PATH_MAX];
+  if(realpath(filename, path) == NULL) {
+    if(error) {
+      *error = errno;
+    }
+    return -1;
+  }
+  if (stat(path, &statbuf) == -1) {
     if(error) {
       *error = errno;
     }
